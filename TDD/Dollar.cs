bd1497a7cfff2ad2d78b4cd1b63a2f756dfbc4cf
Build School <bsdevteam01@build-school.com>
2022-01-29 15:42:46 +0800

    refactor the program to keep the test program passing

--- TDD/Dollar.cs
+++ TDD/Dollar.cs
@@ -2,15 +2,15 @@
 
 public class Dollar
 {
-    public int amount = 10;
+    public int amount;
     
     public Dollar(int amount)
     {
-        
+        this.amount = amount;
     }
 
     public void times(int multiplier)
     {
-        
+        amount *= multiplier;
     }
 }
