c27b2741e018d379addc12cab253432800265cc6
zz0108 <nn12572312@gmail.com>
2022-01-29 16:55:56 +0800

    Move times to Money class and make it abstract

--- TDD/Money.cs
+++ TDD/Money.cs
@@ -1,8 +1,20 @@
 ﻿namespace TDD;
 
-public class Money
+public abstract class Money
 {
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount)
+    {
+        return new Dollar(amount);
+    }
+    
+    public static Money franc(int amount)
+    {
+        return new Franc(amount);
+    }
     
     public override bool Equals(object obj)
     {
--- TDDTest/Test.cs
+++ TDDTest/Test.cs
@@ -9,26 +9,26 @@ public class Tests
     [Test]
     public void TestMaltiplication()
     {
-        Dollar five = new Dollar(5);
-        Assert.AreEqual(new Dollar(10), five.times(2));
-        Assert.AreEqual(new Dollar(15),five.times(3));
+        Money five = Money.dollar(5);
+        Assert.AreEqual(Money.dollar(10), five.times(2));
+        Assert.AreEqual(Money.dollar(15),five.times(3));
     }
     
     [Test]
     public void TestFrancMaltiplication()
     {
-        Franc five = new Franc(5);
-        Assert.AreEqual(new Franc(10), five.times(2));
-        Assert.AreEqual(new Franc(15),five.times(3));
+        Money five = Money.franc(5);
+        Assert.AreEqual(Money.franc(10), five.times(2));
+        Assert.AreEqual(Money.franc(15),five.times(3));
     }
 
     [Test]
     public void TestEquality()
     {
-        Assert.True(new Dollar(5).Equals(new Dollar(5)));
-        Assert.False(new Dollar(5).Equals(new Dollar(6)));
-        Assert.True(new Franc(5).Equals(new Franc(5)));
-        Assert.False(new Franc(5).Equals(new Franc(6)));
-        Assert.False(new Franc(5).Equals(new Dollar(5)));
+        Assert.True(Money.dollar(5).Equals(Money.dollar(5)));
+        Assert.False(Money.dollar(5).Equals(Money.dollar(6)));
+        Assert.True(Money.franc(5).Equals(Money.franc(5)));
+        Assert.False(Money.franc(5).Equals(Money.franc(6)));
+        Assert.False(Money.franc(5).Equals(Money.dollar(5)));
     }
 }
