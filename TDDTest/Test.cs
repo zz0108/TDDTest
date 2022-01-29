9ab31386828fedb6b72cd208899246ed5c5ea8c7
zz0108 <nn12572312@gmail.com>
2022-01-29 15:49:53 +0800

    Allows an object to call the multiply function multiple times

--- TDDTest/Test.cs
+++ TDDTest/Test.cs
@@ -10,7 +10,9 @@ public class Tests
     public void testMaltiplication()
     {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.AreEqual(10, five.amount);
+        Dollar product = five.times(2);
+        Assert.AreEqual(10, product.amount);
+        product = five.times(3);
+        Assert.AreEqual(15,product.amount);
     }
 }
