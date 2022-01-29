f1004658829c8fd5253b6d150bda7133dd70a984
zz0108 <nn12572312@gmail.com>
2022-01-29 17:42:36 +0800

    remove sublayer

--- TDD/Dollar.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TDD;
-
-public class Dollar : Money
-{
-    public Dollar(int amount, string currency) : base(amount, currency)
-    {
-        
-    }
-}
--- TDD/Franc.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TDD;
-
-public class Franc : Money
-{
-    public Franc(int amount, string currency) : base(amount, currency)
-    {
-        
-    }
-}
